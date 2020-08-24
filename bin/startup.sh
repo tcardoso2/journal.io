13e4a1a56fcf3d1bddd0475474671756febb74f9
pi_droneci <tcardoso@gmail.com>
2020-08-24 13:03:14 +0800

    Update of startup command with 'start' cli action

--- bin/startup.sh
+++ bin/startup.sh
@@ -5,6 +5,11 @@
 #
 echo "============================  Starting JOURNAL.io  ============================"
 LOG_LEVEL=info
-./bin/server.js > ./logs/journal.io.log &
+./bin/server.js start > ./logs/journal.io.log &
 echo $! > ./process.pid
 echo "Started with process id: `cat ./process.pid`"
+
+# Process will stay working for one day. If you have a restarting container, that means
+# it will restart after 24h. For now done only for test purpuses, in future might allow
+# this being done via other means
+sleep 1d
